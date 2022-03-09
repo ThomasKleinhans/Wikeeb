eb9577798017c221c2bd06659d682e51e0dfb8d8
ThomasKleinhans <34307673+ThomasKleinhans@users.noreply.github.com>
2022-03-09 18:24:01 +0100

    restore page detail keycap

--- src/pages/KeycapDetails.page.vue
+++ src/pages/KeycapDetails.page.vue
@@ -1,18 +1,58 @@
 <template>
   <q-page padding>
-    <img :src="require('../assets/'+img)" alt="">
+      <div class="fit row wrap justify-center items-start content-start">
+        <div class="col-7">
+        <q-card class="keycaps-card" v-if="item">
+            <q-item>
+                <q-item-section>
+                    <q-item-label>
+                        <q-btn flat round icon="favorite" />
+                        {{item.name}}
+                    </q-item-label>
+                </q-item-section>
+            </q-item>
+            
+            <q-card-section horizontal>
+                    <q-img
+                    src="https://firebasestorage.googleapis.com/v0/b/dev-wikeeb.appspot.com/o/keycaps%2F9o18y7J.jpg?alt=media&token=ffb84685-f816-4d32-9f90-a9ba5585f1d5"
+                    spinner-color="white"
+                    />
+
+                <q-card-section class="col-4">
+                    <q-list>
+                        <q-item clickable v-ripple>
+                            <q-item-section avatar top>
+                                <q-avatar icon="folder" color="primary" text-color="white" />
+                            </q-item-section>
+
+                            <q-item-section>
+                                <q-item-label lines="1">Geekhack</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-card-section>
+            </q-card-section>
+            <q-card-section horizontal>
+                <q-card-section class="col-4">
+                    <q-chip size="12px" color="primary">
+                        PBT
+                    </q-chip>
+                    <q-chip size="12px" color="secondary">
+                        Cherry
+                    </q-chip>
+                </q-card-section>
+            </q-card-section>
+
+        </q-card>
+            {{ $route.params.id }}
+        </div>
+      </div>
   </q-page>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
     export default {
-        props: {
-            img:{
-                type: String,
-                default: 'logo.svg'
-            }
-        },
         data() {
             return {
                 item: null
@@ -25,11 +65,6 @@ import { mapGetters } from 'vuex'
         },
         mounted () {
             this.item = this.getKeycapsById(this.$route.params.id)
-        },
-        methods: {
-            getImgUrl(pic) {
-                return 
-            }
         },
     }
 </script>
